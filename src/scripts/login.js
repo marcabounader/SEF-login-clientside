f3b89259de6b2d0e87696d40910f41d0675070bd
marcabounader92 <marcabounader92@outlook.com>
2023-07-21 10:11:52 +0300

    JS: fixed cookie

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -3,7 +3,7 @@ const register = async (email,password) =>{
     method: "post",
     mode: 'cors',
     cache: "no-cache",
-    origin: 'http://127.0.0.1:5500',
+    origin: 'http://localhost:5500',
     credentials: 'include',
     headers: {
       'Accept': 'application/json',
@@ -32,7 +32,6 @@ document.getElementById('login-btn').addEventListener('click',(e)=>{
         if(data.status==0){
             document.getElementById('error-message').innerText="Failed login. Please check username and password";
         } else{
-            document.cookie = `name=${data.first_name}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/`;
             window.location = "/dashboard.html";
         }
     }).catch((err)=>{
